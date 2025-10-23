48cceef6f4fa66b6e5e8c95ace558144d23580a8
ThreeMonthAgo <225839283+ThreeMonthAgo@users.noreply.github.com>
2025-10-23 23:49:32 +0800

    Added custom control "FloatingBar"

---
+++ Ink_Canvas_Better.Controls/Controls/Panel/FloatingBar/FloatingBar_Properties.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Windows.Controls;
+
+namespace Ink_Canvas_Better.Controls.Panel
+{
+    public partial class FloatingBar
+    {
+        private ObservableCollection<UIElementCollection> _controlsCollection = [];
+        public ObservableCollection<UIElementCollection> ControlsCollection
+        {
+            get => _controlsCollection;
+            set
+            {
+                if (_controlsCollection != null) _controlsCollection.CollectionChanged -= OnControlsCollectionChanged;
+                _controlsCollection = value;
+                if (_controlsCollection != null) _controlsCollection.CollectionChanged += OnControlsCollectionChanged;
+            }
+        }
+
+    }
+}
