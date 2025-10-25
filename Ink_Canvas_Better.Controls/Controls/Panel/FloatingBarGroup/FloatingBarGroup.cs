19320ee9afb900716338a97cbe9246270271efe3
ThreeMonthAgo <225839283+ThreeMonthAgo@users.noreply.github.com>
2025-10-25 14:51:15 +0800

    renamed FloatingBar to FloatingBarGroup and improved it

---
+++ Ink_Canvas_Better.Controls/Controls/Panel/FloatingBarGroup/FloatingBarGroup.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Ink_Canvas_Better.Controls.Panel
+{
+    public partial class FloatingBarGroup : ItemsControl
+    {
+        static FloatingBarGroup()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FloatingBarGroup), new FrameworkPropertyMetadata(typeof(FloatingBarGroup)));
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            ItemsSource = ControlsCollection;
+        }
+    }
+}
---
+++ Ink_Canvas_Better.Controls/Controls/Panel/FloatingBarGroup/FloatingBarGroup_Properties.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Ink_Canvas_Better.Controls.Panel
+{
+    public partial class FloatingBarGroup
+    {
+        public ObservableCollection<Control>? ControlsCollection { get; }
+
+        #region Orientation
+
+
+        public Orientation Orientation
+        {
+            get { return (Orientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
+
+        // It can's be used independently...
+        public static readonly DependencyProperty OrientationProperty =
+            DependencyProperty.Register("Orientation", typeof(Orientation), typeof(FloatingBarGroup), new PropertyMetadata(Orientation.Horizontal));
+
+
+        #endregion
+    }
+}
